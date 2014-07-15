298d42beeb7df6ba2afa9fcbc0b02953dbf351d0
Glen Husman <wappokickingdeer@gmail.com>
2014-07-15 11:13:58 -0700

    Initial commit

---
+++ MyConsole.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmazingDuinoInterface
+{
+    public static class MyConsole
+    {
+        private static Dictionary<int, ConsoleColor> _foregroundChanges = new Dictionary<int, ConsoleColor>();
+        private static Dictionary<int, ConsoleColor> _backgroundChanges = new Dictionary<int, ConsoleColor>();
+
+        public static string ReadLine()
+        {
+            string line = Console.ReadLine();
+            _lines.AppendLine(line);
+
+            return line;
+        }
+
+        public static ConsoleKeyInfo ReadKey()
+        {
+            return ReadKey(false);
+        }
+
+        public static ConsoleKeyInfo ReadKey(bool intercept)
+        {
+            ConsoleKeyInfo info = Console.ReadKey(intercept);
+            if (!intercept)
+            {
+                _lines.Append(info.KeyChar);
+            }
+
+            return info;
+        }
+
+        private static void ReinitializeDictionaries()
+        {
+            _foregroundChanges.Clear();
+            _backgroundChanges.Clear();
+            _foregroundChanges[0] = ConsoleColor.Gray;
+            _backgroundChanges[0] = ConsoleColor.Black;
+        }
+
+        static MyConsole()
+        {
+            ReinitializeDictionaries();
+        }
+
+        private static StringBuilder _lines = new StringBuilder();
+
+        public static ConsoleColor ForegroundColor
+        {
+            get
+            {
+                return Console.ForegroundColor;
+            }
+
+            set
+            {
+                Console.ForegroundColor = value;
+                _foregroundChanges[_lines.Length] = value;
+            }
+        }
+
+        public static ConsoleColor BackgroundColor
+        {
+            get
+            {
+                return Console.BackgroundColor;
+            }
+
+            set
+            {
+                Console.BackgroundColor = value;
+                _backgroundChanges[_lines.Length] = value;
+            }
+        }
+
+        /// <summary>
+        /// Clear the console display WITHOUT CLEARING STREAM HISTORY.
+        /// Using overridden console stream writing methods, such as those implemented by this class, while the display is cleared will produce UNDEFINED RESULTS.
+        /// </summary>
+        public static void ClearDisplay()
+        {
+            Console.Clear();
+        }
+
+        /// <summary>
+        /// Restore the console display from recorded history.
+        /// </summary>
+        public static void RestoreDisplay()
+        {
+            ConsoleColor mainFg = ForegroundColor;
+            ConsoleColor mainBg = BackgroundColor;
+
+            Console.Clear();
+            for (int i = 0; i < _lines.Length; i++)
+            {
+                ConsoleColor foreground;
+                ConsoleColor background;
+
+                if (_foregroundChanges.TryGetValue(i, out foreground))
+                {
+                    Console.ForegroundColor = foreground;
+                }
+
+                if (_backgroundChanges.TryGetValue(i, out background))
+                {
+                    Console.BackgroundColor = background;
+                }
+
+                Console.Write(_lines[i]);
+            }
+
+            Console.ForegroundColor = mainFg;
+            Console.BackgroundColor = mainBg;
+        }
+
+        public static void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+            _lines.AppendLine(line);
+        }
+
+        public static void Write(string line)
+        {
+            Console.Write(line);
+            _lines.Append(line);
+        }
+
+        public static void Clear()
+        {
+            Console.Clear();
+            _lines.Clear();
+            ReinitializeDictionaries();
+        }
+
+        public static void WriteLine(string line, params object[] format)
+        {
+            Console.WriteLine(line, format);
+            _lines.AppendFormat(line + Console.Out.NewLine, format);
+        }
+
+        public static void Write(string line, params object[] format)
+        {
+            Console.Write(line);
+            _lines.AppendFormat(line, format);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace AmazingDuinoInterface
+{
+    class Program
+    {
+        enum MessageType
+        {
+            Standard,
+            Prompt,
+            SerialLog,
+            IncomingMessage
+        }
+
+        class ProgramMustExitException : Exception { }
+
+        static SerialPort arduinoInterface;
+
+        static void Main(string[] args)
+        {
+            MyConsole.WriteLine("Please input serial port name (leave blank for default):");
+            String intName = MyConsole.ReadLine();
+            if (intName.Trim().Length == 0)
+            {
+                intName = "COM3";
+                MyConsole.WriteLine("Defaulting serial port to be {0}", intName);
+            }
+
+            MyConsole.WriteLine("Please input baud rate (leave blank for default):");
+            int baudRate;
+            if (!int.TryParse(MyConsole.ReadLine(), out baudRate))
+            {
+                baudRate = 9600;
+                MyConsole.WriteLine("Defaulting baud rate to be {0}", baudRate);
+            }
+
+            try
+            {
+                arduinoInterface = new SerialPort(intName, baudRate);
+                WriteLine(MessageType.SerialLog, "Opening serial port for communication...");
+                arduinoInterface.Open();
+            }
+            catch (Exception except)
+            {
+                WriteLine(MessageType.SerialLog, "Error opening serial port for communication, aborting program:");
+                MyConsole.WriteLine(except.ToString());
+                MyConsole.ReadKey(true);
+                return;
+            }
+
+            MyConsole.ForegroundColor = ConsoleColor.DarkMagenta;
+            MyConsole.WriteLine("Serial communication prompt is ready, please press enter to continue.");
+            MyConsole.ReadKey(true);
+            MyConsole.Clear();
+
+            ConsoleCommands.Add("help", Help);
+            ConsoleCommandDescriptions.Add("/help", "Displays information for all available commands.");
+            ConsoleCommands.Add("literal", Literal);
+            ConsoleCommandDescriptions.Add("/literal", "Sends a literal string, no comamnds or newlines, to the serial port.");
+            ConsoleCommands.Add("exit", () => { throw new ProgramMustExitException(); });
+            ConsoleCommandDescriptions.Add("/exit", "Exits the program.");
+            //ConsoleCommands.Add("incomingdisplay", InputMessageToggle);
+            //ConsoleCommandDescriptions.Add("/incomingdisplay [state]", "Toggles whether incoming serial port messages are displayed.");
+            ConsoleCommands.Add("input", InputConsoleStateManager);
+            ConsoleCommandDescriptions.Add("/input <view|clear|amount>", "Display or clear the serial input buffer.");
+
+
+            arduinoInterface.DataReceived += new SerialDataReceivedEventHandler(arduinoInterface_DataReceived);
+
+            MyConsole.ForegroundColor = ConsoleColor.Yellow;
+            MyConsole.WriteLine("Welcome to the serial communication prompt. Type /help for help.");
+            MyConsole.ForegroundColor = ConsoleColor.Gray;
+
+            while (true)
+            {
+                MyConsole.ForegroundColor = ConsoleColor.Red;
+                string serialCommand = MyConsole.ReadLine();
+                MyConsole.ForegroundColor = ConsoleColor.Gray;
+                if (serialCommand.StartsWith("/"))
+                {
+                    string[] splitStrings = serialCommand.Substring(1).Split(new char[] { ' ' }, 2);
+                    serialCommand = splitStrings[0];
+                    CurrentCommandArguments = splitStrings.Length > 1 ? splitStrings[1] : null;
+
+                    Action cmd;
+                    if (!ConsoleCommands.TryGetValue(serialCommand, out cmd))
+                    {
+                        WriteLine(MessageType.Standard, "Unknown command. Type /help for help.");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            cmd();
+                        }
+                        catch (ProgramMustExitException)
+                        {
+                            arduinoInterface.Close();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    WriteLine(MessageType.SerialLog, "Sending message to port...");
+                    arduinoInterface.WriteLine(serialCommand);
+                }
+            }
+        }
+
+        static string CurrentCommandArguments;
+
+        static StringBuilder receivedData = new StringBuilder();
+
+        static object synclock = new object();
+
+
+
+        static void InputConsoleStateManager()
+        {
+
+            switch (CurrentCommandArguments == null ? null : CurrentCommandArguments.Trim().ToUpperInvariant())
+            {
+                case "VIEW":
+                    MyConsole.ClearDisplay();
+                    int dispLen = -1;
+                    while (!Console.KeyAvailable)
+                    {
+                        lock (synclock)
+                        {
+                            if (receivedData.Length != dispLen)
+                            {
+                                Console.Clear();
+                                Console.Write(receivedData.ToString());
+                                dispLen = receivedData.Length;
+                            }
+                        }
+                    }
+                    Console.ReadKey(true); // Prevent sending blank message
+                    MyConsole.RestoreDisplay();
+
+                    break;
+                case "CLEAR":
+                    lock (synclock)
+                    {
+                        receivedData.Clear();
+                        MyConsole.WriteLine("Input buffer cleared.");
+                    }
+                    break;
+                case "AMOUNT":
+                    lock (synclock)
+                    {
+                        MyConsole.WriteLine("Buffer contains {0} byte(s) of received data.", receivedData.Length);
+                    }
+                    break;
+                default:
+                    MyConsole.WriteLine("Must specify action: VIEW, CLEAR, or AMOUNT.");
+                    return;
+            }
+        }
+
+        static void arduinoInterface_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            lock (synclock)
+            {
+                int bytesToRead = arduinoInterface.BytesToRead;
+                char[] data = new char[bytesToRead];
+                arduinoInterface.Read(data, 0, bytesToRead);
+                receivedData.Append(data);
+            }
+
+            //lock (synclock)
+            //{
+            //    int bytesToRead = arduinoInterface.BytesToRead;
+            //    char[] data = new char[bytesToRead];
+            //    arduinoInterface.Read(data, 0, bytesToRead);
+            //    string dataStr = new string(data);
+
+            //    while (true)
+            //    {
+            //        int newlinePosition = dataStr.IndexOf("\r\n");
+            //        if (newlinePosition < 0)
+            //        {
+            //            receivedData.Append(dataStr);
+            //            return;
+            //        }
+            //        //else if (newlinePosition == dataStr.Length - 2 /* End of string */)
+            //        //{
+            //        //    receivedData.Append(dataStr, 0, dataStr.Length - 2);
+            //        //    return;
+            //        //}
+            //        else
+            //        {
+            //            receivedData.Append(dataStr, 0, newlinePosition);
+            //            string displayMessage = receivedData.ToString();
+            //            WriteLine(MessageType.IncomingMessage, displayMessage);
+            //            receivedData.Clear();
+            //            dataStr = newlinePosition + 2 >= dataStr.Length ? "" : dataStr.Substring(newlinePosition + 2);
+            //        }
+            //    }
+            //}
+        }
+
+        static void Help()
+        {
+            MyConsole.ForegroundColor = ConsoleColor.Green;
+            MyConsole.WriteLine("Available commands:");
+            foreach (var command in ConsoleCommandDescriptions)
+            {
+                MyConsole.ForegroundColor = ConsoleColor.Magenta;
+                MyConsole.Write(command.Key);
+                MyConsole.ForegroundColor = ConsoleColor.Cyan;
+                MyConsole.Write(" - ");
+                MyConsole.ForegroundColor = ConsoleColor.DarkYellow;
+                MyConsole.WriteLine(command.Value);
+            }
+            MyConsole.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static bool DisplayIncomingMessages = true;
+
+        [Obsolete]
+        static void InputMessageToggle()
+        {
+            if (CurrentCommandArguments == null)
+            {
+                MyConsole.WriteLine("Current incoming message display state: {0}", DisplayIncomingMessages ? "ON" : "OFF");
+                //WriteLine(MessageType.Standard, "Must supply incoming message display state.");
+                return;
+            }
+            switch (CurrentCommandArguments.ToUpperInvariant())
+            {
+                case "TRUE":
+                case "YES":
+                case "ON":
+                    DisplayIncomingMessages = true;
+                    break;
+                case "FALSE":
+                case "NO":
+                case "OFF":
+                    DisplayIncomingMessages = false;
+                    break;
+                default:
+                    MyConsole.WriteLine("Invalid state description.");
+                    return;
+
+            }
+
+            MyConsole.WriteLine("Turned display of incoming messages {0}.", DisplayIncomingMessages ? "on" : "off");
+        }
+
+        static void Literal()
+        {
+            WriteLine(MessageType.Prompt, "Please input literal message to be sent to serial port.");
+            MyConsole.ForegroundColor = ConsoleColor.Red;
+            String input = MyConsole.ReadLine();
+            MyConsole.ForegroundColor = ConsoleColor.Gray;
+            WriteLine(MessageType.SerialLog, "Sending literal message to serial port without newline...");
+            arduinoInterface.Write(input);
+        }
+
+        static Dictionary<String, Action> ConsoleCommands = new Dictionary<string, Action>(StringComparer.OrdinalIgnoreCase);
+        static Dictionary<String, String> ConsoleCommandDescriptions = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        static void WriteLine(MessageType type, string message)
+        {
+            switch (type)
+            {
+                case MessageType.Prompt:
+                case MessageType.Standard:
+                    break;
+                case MessageType.SerialLog:
+                    MyConsole.ForegroundColor = ConsoleColor.DarkGray;
+                    MyConsole.Write("[Serial] ");
+                    break;
+                case MessageType.IncomingMessage:
+                    if (!DisplayIncomingMessages)
+                    {
+                        return;
+                    }
+
+                    MyConsole.ForegroundColor = ConsoleColor.DarkGray;
+                    MyConsole.Write("[Incoming Message] ");
+                    break;
+            }
+
+            MyConsole.ForegroundColor = ConsoleColor.Gray;
+            MyConsole.WriteLine(message);
+            MyConsole.ForegroundColor = ConsoleColor.Red;
+        }
+    }
+}
